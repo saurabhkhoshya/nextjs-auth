b56f7494788e116ec7d2ad95f1c8db48b011eeef
unknown <saurabhkhoshya@gmail.com>
2021-10-23 18:12:04 +0530

    change password

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,8 +1,54 @@
-import { useState } from 'react';
-import classes from './auth-form.module.css';
+import { useState, useRef } from "react";
+import { signIn } from "next-auth/client";
+import { useRouter } from "next/router";
+import classes from "./auth-form.module.css";
+
+async function createUser(email, password) {
+  const response = await fetch("/api/auth/signup", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong");
+  }
+  return data;
+}
 
 function AuthForm() {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
+  async function submitHandler(event) {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    if (isLogin) {
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword,
+      });
+      if (!result.error) {
+        // set some auth state
+        router.replace("/profile");
+      }
+    } else {
+      try {
+        const result = await createUser(enteredEmail, enteredPassword);
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
@@ -10,24 +56,29 @@ function AuthForm() {
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,24 @@
-import UserProfile from '../components/profile/user-profile';
+import { getSession } from "next-auth/client";
+import { useContext } from "react";
+import UserProfile from "../components/profile/user-profile";
 
 function ProfilePage() {
   return <UserProfile />;
 }
 
+export async function getServerSideProps() {
+  const session = await getSession({ req: useContext.req });
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+}
 export default ProfilePage;
